7cf36a12deef5fa9be677bcf145811e81083ed81
IkhsanFadhilah <muhammad21045@mail.unpad.ac.id>
2024-06-20 16:54:55 +0700

    Commit Custom View

--- SkiVent/app/build.gradle.kts
+++ SkiVent/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("kotlin-parcelize")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -49,4 +51,25 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("androidx.paging:paging-runtime-ktx:3.1.1")
+    implementation ("androidx.room:room-paging:2.5.1")
+
+    implementation ("androidx.room:room-runtime:2.5.1")
+    kapt ("androidx.room:room-compiler:2.5.1")
+    implementation ("androidx.room:room-ktx:2.5.1")
+
+    implementation ("com.github.bumptech.glide:glide:4.15.0")
+    implementation ("id.zelory:compressor:3.0.1")
+
+    // api
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.5")
+
+    // core
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
 }
---
+++ SkiVent/app/src/main/java/com/dicoding/skivent/dataclass/LoginRegisterAccount.kt
@@ -0,0 +1,25 @@
+package com.dicoding.skivent.dataclass
+
+data class RegisterDataAccount(
+    var name: String,
+    var email: String,
+    var password: String
+)
+data class LoginDataAccount(
+    var email: String,
+    var password: String
+)
+data class ResponseDetail(
+    var error: Boolean,
+    var message: String
+)
+data class ResponseLogin(
+    var error: Boolean,
+    var message: String,
+    var loginResult: LoginResult
+)
+data class LoginResult(
+    var userId: String,
+    var name: String,
+    var token: String
+)
