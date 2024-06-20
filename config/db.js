f80e1a1d400a0c59e976ad984fb901c40acd6a50
Jihad Amal Farid <jihad.amalfarid@gmail.com>
2024-06-21 01:07:28 +0700

    Added API PUT user profile

--- config/db.js
+++ config/db.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://admin:capstone123@capstone.8eh9gyx.mongodb.net/skivent?retryWrites=true&w=majority&appName=Capstone', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected successfully');
-    } catch (error) {
-        console.error('MongoDB connection failed:', error);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
-=======
 const mysql = require("mysql2");
 require("dotenv").config();
 
@@ -27,4 +9,3 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool.promise();
->>>>>>> 3a86f8d (Added loadmodel)
